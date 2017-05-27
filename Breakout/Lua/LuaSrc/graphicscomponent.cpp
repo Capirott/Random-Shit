16ea721c66cc4350e548c4c5cb9332c0ce774f42
Erickson <erickson.lucas@ive.com>
2017-05-27 12:03:07 -0300

    commiting breakout files for starting the project

---
+++ Breakout/Lua/LuaSrc/graphicscomponent.cpp
@@ -0,0 +1,14 @@
+#include "graphicscomponent.h"
+#include "component.h"
+#include <LuaBridge.h>
+#include <iostream>
+ 
+GraphicsComponent::GraphicsComponent(luabridge::LuaRef& componentTable) {
+    using namespace luabridge;
+    auto filenameRef = componentTable["filename"];
+    if (filenameRef.isString()) {
+        filename = filenameRef.cast<std::string>();
+    } else {
+        std::cout << "Error, GraphicsComponent.filename is not a string!" << std::endl;
+    }
+}
