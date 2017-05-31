1b67ea05d18cd1c95b177b4401693c25e39746b3
Erickson <erickson.lucas@ive.com>
2017-05-31 10:27:23 -0300

    Made some changes in the view and templates

--- lgpjc/lgtrp/urls.py
+++ lgpjc/lgtrp/urls.py
@@ -6,6 +6,8 @@ app_name = 'lgtrp'
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^student/(?P<pk>[0-9]+)/$', views.StudentView.as_view(), name='student'),
-    url(r'^school/(?P<pk>[0-9]+)/$', views.SchoolView.as_view(), name='school'),
+    url(r'^student/$', views.StudentView.as_view(), name='student'),
+    url(r'^school/$', views.SchoolView.as_view(), name='school'),
+    url(r'^student/(?P<pk>[0-9]+)/$', views.StudentInfoView.as_view(), name='student_info'),
+    url(r'^school/(?P<pk>[0-9]+)/$', views.SchoolInfoView.as_view(), name='school_info'),
 ]
--- lgpjc/lgtrp/views.py
+++ lgpjc/lgtrp/views.py
@@ -2,20 +2,42 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.contrib import messages
 
 from .models import School, Student
 
-class IndexView(generic.ListView):
+class IndexView(generic.TemplateView):
     template_name = 'lgtrp/index.html'
-    context_object_name = 'school_list'
-    
-    def get_queryset(self):
-        return School.objects.order_by('-name')
 
-class StudentView(generic.DetailView):
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        messages.info(self.request, 'Welcome stranger!')
+        return context
+        
+class StudentInfoView(generic.DetailView):
     model = Student
-    template_name = 'lgtrp/student.html'
+    template_name = 'lgtrp/student_info.html'
 
-class SchoolView(generic.DetailView):
+class SchoolInfoView(generic.DetailView):
     model = School
-    template_name = 'lgtrp/school.html'
+    template_name = 'lgtrp/school_info.html'
+
+class StudentView(generic.ListView):
+	context_object_name = 'student_list'
+	template_name = 'lgtrp/student.html'
+
+	def get_queryset(self):
+        	return School.objects.order_by('-name')
+
+class SchoolView(generic.ListView):
+	model = School
+	context_object_name = 'school_list'
+	template_name = 'lgtrp/school.html'
+	def get_queryset(self):
+		return School.objects.order_by('-name')
+
+
+class StudentCreateView(generic.CreateView):
+	model = Student
+	fields = ['name']
+
