86e6713edaf32f722f5887e378437d59455b1c46
Erickson <erickson@live.com>
2017-05-31 20:18:35 -0300

    Models and views changed

--- lgpjc/lgtrp/models.py
+++ lgpjc/lgtrp/models.py
@@ -3,13 +3,13 @@ from django.db import models
 from django.utils import timezone
 
 class Student(models.Model):
-    name = models.CharField(max_length=256)
-    registration_date = models.DateTimeField('Student Registration Date')
+    name = models.CharField(max_length=256, null=False, unique=True)
+    registration_date = models.DateTimeField('Registration Date', default=timezone.now)
     GENDERS = (
         ('M', 'Male'),
         ('F', 'Female'),
     )
-    gender = models.CharField(max_length=1, choices=GENDERS)
+    gender = models.CharField(max_length=1, choices=GENDERS, null=False)
 
     def __str__(self):
         return self.name
@@ -17,11 +17,22 @@ class Student(models.Model):
     def was_registered_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=30) <= self.registration_date <= now
+        
+    def get_absolute_url(self):
+        return u'/lgtrp/student/edit/%d' % self.id 
     
 class School(models.Model):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, unique=True, null=False)
     students = models.ManyToManyField(Student)
+    registration_date = models.DateTimeField('Registration Date', default=timezone.now, null=False)
     
     def __str__(self):
         return self.name
+        
+    def get_absolute_url(self):	
+        return u'/lgtrp/school/edit/%d' % self.id 
+        
+    def was_registered_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=30) <= self.registration_date <= now
 
